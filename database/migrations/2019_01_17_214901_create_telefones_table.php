e6fe5e62874cf98130ca17dc133205545c9f0dff
Gabriel Schenato <gabrielschenato152@hotmail.com>
2019-01-17 19:56:41 -0200

    Criando Migrations

---
+++ database/migrations/2019_01_17_214901_create_telefones_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTelefonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telefones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('descricao', 50);
+            $table->integer('codpais');
+            $table->integer('ddd');
+            $table->integer('prefixo');
+            $table->integer('sufixo');
+            $table->timestamps();
+            
+            $table->unsignedInteger('pessoa_id');
+            $table->foreign('pessoa_id')->references('id')->on('pessoas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telefones');
+    }
+}
