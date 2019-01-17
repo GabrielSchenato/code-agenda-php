4100af05d11967d0e6eb56863019feecde806d82
Gabriel Schenato <gabrielschenato152@hotmail.com>
2019-01-17 21:25:26 -0200

    Instalando Dependências

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var elixir = require('laravel-elixir'),
+        bowerDir = "vendor/bower_components/";
+elixir(function (mix) {
+    mix.copy(bowerDir + 'bootstrap/fonts', 'public/fonts')
+            .copy(bowerDir + 'fontawesome/fonts', 'public/fonts')
+    
+            .copy(bowerDir + 'bootstrap/dist/bootstrap.js', 'resources/assets/js')
+            .copy(bowerDir + 'jquery/dist/jquery.js', 'resources/assets/js')
+            .scripts([
+                'jquery.js',
+                'bootstrap.js'
+            ], 'public/js/scripts.js')
+            .less('app.less');
+});
